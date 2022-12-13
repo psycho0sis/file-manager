9c4823bbdbdce01fd733e6893accd0b5e1386006
ilonaK <ikosko@alfabank.ru>
2022-12-13 17:06:13 +0300

    refactor: rewrite colorize functions

--- src/utils/colorize.js
+++ src/utils/colorize.js
@@ -1,11 +1,5 @@
-export const colorizeInYellow = (string) => {
-  return `\x1b[33m${string} \x1b[0m`;
-};
+export const colorizeInYellow = (string) => `\x1b[33m${string} \x1b[0m`;
 
-export const colorizeInRed = (string) => {
-  return `\x1b[91m${string} \x1b[0m`;
-};
+export const colorizeInCyan = (string) => `\x1b[36m${string} \x1b[0m`;
 
-export const colorizeInCyan = (string) => {
-  return `\x1b[36m${string} \x1b[0m`;
-};
+export const colorizeInRed = (string) => `\x1b[91m${string} \x1b[0m`;
