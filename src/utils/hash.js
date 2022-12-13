081536cff22b45610ff67559429f3d88bb212429
ilonaK <ikosko@alfabank.ru>
2022-12-13 17:06:40 +0300

    feat: add hash function

---
+++ src/utils/hash.js
@@ -0,0 +1,19 @@
+import path from "path";
+import { createHash } from "crypto";
+import { readFile } from "fs/promises";
+
+import { ERROR } from "./constants.js";
+import { colorizeInRed } from "./colorize.js";
+
+export const calculateHash = async (currentPath, pathToFile) => {
+  const pathToTheFile = path.join(currentPath, pathToFile);
+
+  try {
+    const data = await readFile(pathToTheFile, { encoding: "utf-8" });
+    const hex = createHash("sha256").update(data).digest("hex");
+
+    console.log(`Hash was created: ${hex}`);
+  } catch {
+    console.error(colorizeInRed(ERROR));
+  }
+};
