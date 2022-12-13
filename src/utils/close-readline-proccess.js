f8e3d0151849ce592e2f4474b5ca24deaf45978b
ilonaK <ikosko@alfabank.ru>
2022-12-13 17:06:25 +0300

    refactor: refactor closeReadlineProcess function

--- src/utils/close-readline-proccess.js
+++ src/utils/close-readline-proccess.js
@@ -1,10 +1,14 @@
 import { sayGoodBuyToUser } from "./say-bye-to-user.js";
+import { colorizeInRed } from "./colorize.js";
 
 export const closeReadlineProcess = async (rl) => {
-  rl.question("Are you sure you want to exit? ", async (answer) => {
-    if (answer.match(/^y(es)?$/i)) {
-      await sayGoodBuyToUser();
-      rl.close();
+  rl.question(
+    colorizeInRed("Are you sure you want to exit? (y/n) "),
+    async (answer) => {
+      if (answer.match(/^y(es)?$/i)) {
+        await sayGoodBuyToUser();
+        rl.close();
+      }
     }
-  });
+  );
 };
