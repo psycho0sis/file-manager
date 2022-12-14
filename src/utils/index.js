a4f5737d022adef5751f3c5200be80f6b1fbf640
ilonaK <ikosko@alfabank.ru>
2022-12-14 13:56:57 +0300

    fix: fix imports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,12 @@
-import { getFilesAndFoldersInCurrentDirectory } from "./get-files-and-folders-in-current-directory.js";
-import { colorizeInYellow, colorizeInCyan, colorizeInRed } from "./colorize.js";
-import { getCommandAndArguments } from "./get-command-and-arguments.js";
-import { defineCurrentDirectory } from "./define-current-directory.js";
-import { closeReadlineProcess } from "./close-readline-process.js";
-import { sayHelloToUser } from "./say-hello-to-user.js";
-import { sayGoodBuyToUser } from "./say-bye-to-user.js";
-import { printOSInformation } from "./os/index.js";
-import { runCommand } from "./run-command.js";
-import { calculateHash } from "./hash.js";
-import { compare } from "./compare.js";
-import { isFile } from "./is-file.js";
-
-export {
-  defineCurrentDirectory,
-  closeReadlineProcess,
-  calculateHash,
-  colorizeInYellow,
-  colorizeInCyan,
-  colorizeInRed,
-  compare,
-  getFilesAndFoldersInCurrentDirectory,
-  getCommandAndArguments,
-  isFile,
-  runCommand,
-  printOSInformation,
-  sayHelloToUser,
-  sayGoodBuyToUser,
-};
+export * from "./get-files-and-folders-in-current-directory.js";
+export * from "./colorize.js";
+export * from "./get-command-and-arguments.js";
+export * from "./define-current-directory.js";
+export * from "./close-readline-process.js";
+export * from "./say-hello-to-user.js";
+export * from "./say-bye-to-user.js";
+export * from "./os/index.js";
+export * from "./run-command.js";
+export * from "./hash.js";
+export * from "./compare.js";
+export * from "./is-file.js";
