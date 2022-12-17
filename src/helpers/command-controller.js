ecb8a068d19e0569100e0c81fbf34b5abbda900a
ilonaK <ikosko@alfabank.ru>
2022-12-17 13:25:47 +0300

    refactor: delete console.log

--- src/helpers/command-controller.js
+++ src/helpers/command-controller.js
@@ -32,7 +32,6 @@ const runCommand = async (command, commands, args = null, rl) => {
     if (!COMMANDS_WITHOUT_ARGS.includes(command) && !args) {
       printErrorAndPrompt(ERRORS.ERROR_ABOUT_REQUIRED_ARGUMENTS, rl);
     } else if (COMMANDS_WITH_ONE_ARG.includes(command) && argsLength === 1) {
-      console.log(args);
       await commands[command](args[0], command).catch(() => {
         printErrorAndPrompt(ERRORS.DEFAULT_ERROR, rl);
       });
